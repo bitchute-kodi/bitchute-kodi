2d3414f00a23c84f2f9b54f40f070fa94f440bff
Jason Francis <jkfranci@iu.edu>
2017-10-09 23:49:22 -0400

    The basics of getting beautifulsoup (bs4) running.

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('https://api.github.com/events')
-print(r.text)
+r = requests.get('https://www.bitchute.com/InRangeTV/')
+soup = BeautifulSoup(r.text, 'html.parser')
+print(soup.prettify())
