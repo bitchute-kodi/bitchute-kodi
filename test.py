e99fef0bb3c20697fb7dd5532f9af41e41b6ee5e
Jason Francis <jkfranci@iu.edu>
2017-10-13 21:55:02 -0400

    Passing in a larger container when creating a VideoLink so we can find info about the thumbnail image, too.

--- test.py
+++ test.py
@@ -6,10 +6,22 @@ from bs4 import BeautifulSoup
 baseUrl = "https://www.bitchute.com"
 
 class VideoLink:
-	def __init__(self, linkSoup):
+	def __init__(self, containerSoup):
+		titleDiv = containerSoup.findAll('div', "channel-videos-title")[0]
+		linkSoup = titleDiv.findAll('a')[0]
+		
 		self.title = linkSoup.string
 		self.pageUrl = linkSoup.get("href")
 		self.id = self.pageUrl.split("/")[-1]
+		self.thumbnail = None
+		#before we can find thumnails let's strip out play button images.
+		for playButton in containerSoup.findAll('img', "play-overlay-icon"):
+			playButton.extract()
+		
+		thumbnailMatches = containerSoup.findAll('img', "img-responsive")
+		
+		if thumbnailMatches:
+			self.thumbnail = baseUrl + thumbnailMatches[0].get("src")
 
 	def getUrl(self, channelId):
 		return(baseUrl + "/torrent/" + channelId + "/" + self.id + ".torrent")
@@ -21,9 +33,9 @@ class Channel:
 
 		r = requests.get(baseUrl + "/" + self.channelName)
 		soup = BeautifulSoup(r.text, 'html.parser')
-		for div in soup.findAll('div', "channel-videos-title"):
-			for link in div.findAll('a'):
-				self.videos.append(VideoLink(link))
+
+		for videoContainer in soup.findAll('div', "channel-videos-container"):
+			self.videos.append(VideoLink(videoContainer))
 
 		# for now I only know how to find the ID from a video, so take the last item
 		# in videos and find the channel's ID.
@@ -38,5 +50,5 @@ x = Channel("InRangeTV")
 print(x.channelName + " (" + x.id + ")")
 print("Videos:")
 for video in x.videos:
-	print(video.title + "\n" + video.getUrl(x.id))
+	print(video.title + "\n" + video.thumbnail + "\n" + video.getUrl(x.id) + "\n")
 
