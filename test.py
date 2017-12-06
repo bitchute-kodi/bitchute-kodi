2b949fd1566f61fd7b389dcddb6a144b9469dfa9
Jason Francis <jkfranci@iu.edu>
2017-12-05 23:51:59 -0500

    Stripping out a second fetchLoggedIn() we don't need.

--- test.py
+++ test.py
@@ -49,15 +49,7 @@ class Channel:
 		self.hasPrevPage = False
 		self.hasNextPage = False
 
-		self.setThumbnail()
 		self.setPage(self.page)
-
-	def setThumbnail(self):
-		thumbnailReq = fetchLoggedIn(baseUrl + "/channel/" + self.channelName)
-		thumbnailSoup = BeautifulSoup(thumbnailReq.text, 'html.parser')
-		thumbnailImages = thumbnailSoup.findAll("img", id="fileupload-medium-icon-2")
-		if thumbnailImages:
-			self.thumbnail = baseUrl + thumbnailImages[0].get("src")
 
 	def setPage(self, pageNumber):
 		self.videos = []
@@ -66,8 +58,15 @@ class Channel:
 		self.hasPrevPage = False
 		self.hasNextPage = False
 		
-		r = postLoggedIn(baseUrl + "/channel/" + self.channelName + "/extend/", baseUrl + "/channel/" + self.channelName + "/",{"offset": 10 * (self.page - 1)})
+		if self.page == 1:
+			r = fetchLoggedIn(baseUrl + "/channel/" + self.channelName)
+		else:
+			r = postLoggedIn(baseUrl + "/channel/" + self.channelName + "/extend/", baseUrl + "/channel/" + self.channelName + "/",{"offset": 10 * (self.page - 1)})
 		soup = BeautifulSoup(r.text, 'html.parser')
+		
+		thumbnailImages = soup.findAll("img", id="fileupload-medium-icon-2")
+		if thumbnailImages and thumbnailImages[0].has_attr("data-src"):
+			self.thumbnail = baseUrl + thumbnailImages[0].get("data-src")
 		
 		for videoContainer in soup.findAll('div', "channel-videos-container"):
 			self.videos.append(VideoLink(videoContainer))
